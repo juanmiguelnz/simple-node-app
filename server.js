ddcdb16d699ac1dbc5ddd5e6d25a362aefd71768
miguel <miguelkarlojuan@gmail.com>
2022-07-14 20:35:41 +1200

    rename file

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("WELCOME TO YOUR WEBPAGE v3!");
+});
+
+app.listen(8080, () => {
+    console.log("Hello World!");
+});
